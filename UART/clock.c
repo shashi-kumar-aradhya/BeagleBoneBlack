e3bf70722e1b0b874fb59cca16b1f4004aa2ab27
shashi-kumar-aradhya <shashikumararadhya@gmail.com>
2017-03-18 13:22:47 +0530

    Added control module registers

--- UART/clock.c
+++ UART/clock.c
@@ -1,7 +1,21 @@
 #include "clock.h"
 
-void CM_perSetClock(CLK_MODULE_t module, CM_MODULE_REG reg, unsigned int value)
+void CM_SetClk(CLK_MODULE_t module, CM_MODULE_REG reg, unsigned int value)
 {
 
+	PUT32(module+reg, value);
+}
+
+unsigned int CM_GetClk(CLK_MODULE_t module, CM_MODULE_REG reg)
+{
+	return GET32(module+reg);
+}
+
+void Init_UART0_clk()
+{
+	CM_SetClk(CM_PER, CM_PER_GPIO1_CLKCTRL, ((1 << 18) | 0x2));			//Setting GPIO1 CLK to 0x40002
+	while ((CM_GetClk(CM_PER, CM_PER_GPIO1_CLKCTRL) & (0x3 << 16)) != 0 ) ;		//Wait till clock becomes fully functional
 
 }
+
+
--- UART/uart_console.c
+++ UART/uart_console.c
@@ -6,7 +6,13 @@
 /*	(*PUT32(CM_PER_BASE+CM_PER_GPIO1)) = 1<<18 | 2;
 	ra = GET32(GPIO1_BASE+GPIO_OE);
 */
-
+int UART_init(unsigned int baudrate, STOP_BIT_t stopBit, PARITY_BIT_t parity, FLOW_flowControl) 
+{
+	//UART mux config
+	Uart0_Mux();
+	//Init clock and muxing of pins
+	Init_UART0_Clock();
+}
 void UART_putc(char c);
 char UART_getc();
 int UART_putstring(char *str, unsigned int length);
