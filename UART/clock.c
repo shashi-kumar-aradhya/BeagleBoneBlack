e2288fe7ef02be468041e3e45f01b8d0fb15855b
shashi-kumar-aradhya <shashikumararadhya@gmail.com>
2017-03-19 09:47:30 +0530

    Clocks addition
    Signed-off-by: shashi-kumar-aradhya <shashikumararadhya@gmail.com>

--- UART/clock.c
+++ UART/clock.c
@@ -1,23 +1,31 @@
 #include "clock.h"
 
-void CM_SetClk(CLK_MODULE_t module, CM_MODULE_REG reg, unsigned int value)
+void CM_SetClk(CLK_MODULE_t module, CM_PER_REG reg, unsigned int value)
 {
 
 	PUT32(module+reg, value);
 }
 
-unsigned int CM_GetClk(CLK_MODULE_t module, CM_MODULE_REG reg)
+unsigned int CM_GetClk(CLK_MODULE_t module, CM_PER_REG reg)
 {
 	return GET32(module+reg);
 }
 
 void Init_UART0_clk()
 {
-	CM_SetClk(CM_PER, CM_PER_GPIO1_CLKCTRL, ((1 << 18) | 0x2));			//Setting GPIO1 CLK to 0x40002
-//	while ((CM_GetClk(CM_PER, CM_PER_GPIO1_CLKCTRL) & (0x3 << 16)) != 0 ) ;		//Wait till clock becomes fully functional
-	
 
-	CM_SetClk(CM_PER, CM_PER_L4HS_CLKCTRL, ((1 << 18) | 0x2));			//Setting GPIO1 CLK to 0x40002
+	unsigned int temp;
+	 // software-forced wake-up transition on the "always on clock domain", TRM Table 8-92
+	temp = CM_GetClk(CM_WKUP,CM_WKUP_CLKSTCTRL);		//This register enables the domain power state transition. It controls the SW supervised clock domain state transition between ON-ACTIVE and ON-INACTIVE states. It also hold one status bit per clock input of the domain
+	temp &= ~(0x3);
+	temp |= (0x3);
+	CM_SetClk(CM_PER, CM_PER_CLKSTCTRL, temp);		//0x2=SW_WKUP
+
+
+	temp = CM_GetClk(CM_PER, CM_PER,L4HS_CLKSTCTRL);	//This register enables the domain power state transition. It controls the SW supervised clock domain state transition between ON-ACTIVE and ON-INACTIVE states. It also hold one status bit per clock input of the domain
+	temp &= ~(0x3);
+	temp |= (0x3);
+	CM_SetClk(CM_PER, CM_PER_L4HS_CLKSTCTRL, temp);		//0x2=SW_WKUP
    
 
 }
