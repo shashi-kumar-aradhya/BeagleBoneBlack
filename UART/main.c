c306c053ddb8f745992a8257a4dc68b049548039
shashi-kumar-aradhya <shashikumararadhya@gmail.com>
2017-04-01 14:13:30 +0530

    Moved gpio functionalities to gpio files

--- UART/main.c
+++ UART/main.c
@@ -2,28 +2,11 @@
 #include "uart_console_header.h"
 #include "clock.h"
 #include "types.h"
-
-#define GPIO1_BASE		 0X4804C000
-#define GPIO_OE  		 0X134
-#define GPIO_SETDATAOUT 	 0X194
-
-#define CM_PER_BASE 		 0x44e00000
-#define CM_PER_GPIO1		 0xac
-
+#include "gpio.h"
 
 void _main(void)
 {
-
-
-	volatile unsigned int ra;
-	char input;
-
-	PUT32(CM_PER_BASE+CM_PER_GPIO1, 1<<18 | 2);
-	ra = GET32(GPIO1_BASE+GPIO_OE);
-	ra &= ~(1<<21);
-	PUT32(GPIO1_BASE+GPIO_OE, ra);
-
-	PUT32(GPIO1_BASE+GPIO_SETDATAOUT, 1<<21);
+	LED_init();
 
 	UART_init(115200,STOP1,PARITY_NONE,FLOW_OFF);
 	UART_putC('\n');
