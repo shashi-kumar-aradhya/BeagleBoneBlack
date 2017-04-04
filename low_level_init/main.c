24dda03e5d7083593338a974cde0a08260ae584b
shashi-kumar-aradhya <shashikumararadhya@gmail.com>
2017-04-04 21:34:26 +0530

    Implementation of Printf
    Signed-off-by: shashi-kumar-aradhya <shashikumararadhya@gmail.com>

---
+++ low_level_init/main.c
@@ -0,0 +1,30 @@
+#include "uart.h"
+#include "uart_console_header.h"
+#include "clock.h"
+#include "types.h"
+#include "gpio.h"
+
+extern unsigned short int value;
+
+void _main(void)
+{
+	LED_init();
+
+	UART_init(115200,STOP1,PARITY_NONE,FLOW_OFF);
+	UART_putC('\n');
+	UART_putString("Uart init done : ");
+	UART_putString("Hi shashi\n");
+	UART_putString("Uart init done : ");
+	UART_putString("Hi dabba\n\n");
+	//UART_putString("LCR value = ");
+
+
+
+/*
+	while (1) {
+		UART_putString("Enter a character : ");
+		UART_putC(UART_getC());
+		UART_putC('\n');
+	}
+*/
+}
