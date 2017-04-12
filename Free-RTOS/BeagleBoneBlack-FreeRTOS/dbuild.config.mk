c8fb07e11d504727e2a9a2bd7d4a25c0c00bcd3f
shashi-kumar-aradhya <shashikumararadhya@gmail.com>
2017-04-12 08:21:54 +0530

    Changes in freertos
    Signed-off-by: shashi-kumar-aradhya <shashikumararadhya@gmail.com>

--- Free-RTOS/BeagleBoneBlack-FreeRTOS/dbuild.config.mk
+++
@@ -1,9 +0,0 @@
-
-CFLAGS += -mcpu=cortex-a8 -Wall -Wextra -Wshadow -Wpointer-arith -Wbad-function-cast -Wcast-align -Wsign-compare \
-                -Waggregate-return -Wstrict-prototypes -Wmissing-prototypes -Wmissing-declarations -Wunused
-	
-CFLAGS += -I $(BASE)FreeRTOS/Source/portable/GCC/RaspberryPi/
-CFLAGS += -I $(BASE)FreeRTOS/Source/include/
-CFLAGS += -I $(BASE)Demo/Drivers/
-
-TOOLCHAIN=arm-none-eabi-
